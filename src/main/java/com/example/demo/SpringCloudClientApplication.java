94aee6dd01fe4f1877c2f36711e78d1491721a48
00850828 <ankit.shah@aa.com>
2018-01-30 14:01:13 -0600

    Added server behavior

--- src/main/java/com/example/demo/SpringCloudClientApplication.java
+++ src/main/java/com/example/demo/SpringCloudClientApplication.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringCloudClientApplication {
@@ -9,4 +13,17 @@ public class SpringCloudClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudClientApplication.class, args);
 	}
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+ 
+    @Value("${msg:Hello world - Config Server is not working..pelase check}")
+    private String msg;
+ 
+    @RequestMapping("/msg")
+    String getMsg() {
+        return this.msg;
+    }
 }
